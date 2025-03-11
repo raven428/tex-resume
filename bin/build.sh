44e0f8a456e3c773565284481a5fc9061215dbaa
Dmitry Sukhodoev <raven428@gmail.com>
2025-03-11 15:35:46 +0500

    update build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ set -ueo pipefail
 MY_BIN="$(readlink -f "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
 if [[ "$(
-  /usr/bin/env docker inspect "${TEXLIVE_CONT_NAME}" -f "
+  /usr/bin/env podman inspect "${TEXLIVE_CONT_NAME}" -f "
     {{- range .Mounts -}}
       {{- if eq .Destination \"/workdir\" -}}
         {{- .Source -}}{{- break -}}
@@ -15,21 +15,21 @@ if [[ "$(
   " 2>/dev/null || true
 )" != "$(pwd)" ]]; then
   echo "path changed, destroying container…"
-  /usr/bin/env docker rm -f "${TEXLIVE_CONT_NAME}" 2>/dev/null || true
+  /usr/bin/env podman rm -f "${TEXLIVE_CONT_NAME}" 2>/dev/null || true
 fi
 if [[ "$(
-  /usr/bin/env docker container inspect -f '{{.State.Status}}' \
+  /usr/bin/env podman container inspect -f '{{.State.Status}}' \
     "${TEXLIVE_CONT_NAME}" 2>/dev/null || true
 )" != "running" ]]; then
   # shellcheck disable=2086
   /usr/bin/env \
-    docker run -it -d --name="${TEXLIVE_CONT_NAME}" --rm \
+    podman run -it -d --name="${TEXLIVE_CONT_NAME}" --rm \
     -v "$(pwd):/workdir" ghcr.io/raven428/container-images/texlive-myminimal:latest \
     ${TEXLIVE_CONT_COMMAND}
   /usr/bin/env
-  docker exec "${TEXLIVE_CONT_NAME}" rm -rfv \
-    /usr/local/texlive/2024/texmf-dist/fonts/type1/adobe
+  podman exec "${TEXLIVE_CONT_NAME}" bash -c "rm -rfv \
+  /usr/local/texlive/*/texmf-dist/fonts/type1/adobe"
 fi
 for texfile in "${MY_PATH}/../"*.tex; do
-  docker exec -w /workdir "${TEXLIVE_CONT_NAME}" xelatex "$(basename "${texfile}")"
+  podman exec -w /workdir "${TEXLIVE_CONT_NAME}" xelatex "$(basename "${texfile}")"
 done
